5b2816cb2d2bc854f1ecf33d4d1deb4264328e9c
wangchan <dacn2017@gmail.com>
2021-01-05 18:17:14 +0900

    HW4 fin

--- Standford_Matlab/ex4-Regularized Linear Regression and  Bias/ex4/nnCostFunction.m
+++ Standford_Matlab/ex4-Regularized Linear Regression and  Bias/ex4/nnCostFunction.m
@@ -3,7 +3,7 @@ function [J grad] = nnCostFunction(nn_params, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
-%NNCOSTFUNCTION Implements the neural network cost function for a two layer
+%% NNCOSTFUNCTION Implements the neural network cost function for a two layer
 %neural network which performs classification
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
@@ -62,24 +62,49 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+%% forward Propagation
+a1 = [ones(m, 1) X];
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1) a2];
 
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+hThetaX = a3;
 
+yVec = zeros(m,num_labels);
 
+for i = 1:m
+    yVec(i,y(i)) = 1;
+end
 
+%% BackPropagation
+J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1-yVec) .* log(1-hThetaX)));
 
+regularator = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
 
+J = J + regularator;
 
+for t = 1:m
+	a1 = [1; X(t,:)'];
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
 
+	yy = ([1:num_labels]==y(t))';
+	delta_3 = a3 - yy;
 
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end);
 
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
 
-
-
-
-
-
-
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 % -------------------------------------------------------------
 
 % =========================================================================
