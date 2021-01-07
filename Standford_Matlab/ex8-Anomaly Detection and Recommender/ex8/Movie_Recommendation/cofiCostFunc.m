0d23e9d8b1654e2cc32f19afb31d84bf07d5d227
wangchan <dacn2017@gmail.com>
2021-01-07 12:57:25 +0900

    HW8 part2 fin

--- Standford_Matlab/ex8-Anomaly Detection and Recommender/ex8/Movie_Recommendation/cofiCostFunc.m
+++ Standford_Matlab/ex8-Anomaly Detection and Recommender/ex8/Movie_Recommendation/cofiCostFunc.m
@@ -39,20 +39,13 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+mul=X*Theta';
+diff=(mul-Y).*R;
+J=sum(sum((diff).^2));
+J=J/2+(lambda/2)*sum(sum(X.^2))+(lambda/2)*sum(sum(Theta.^2));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+X_grad=(diff*Theta)+ (lambda*X);
+Theta_grad=(diff'*X)+(lambda*Theta);
 
 
 % =============================================================
